eee4088be052205607780b6b3c54964b32ca6ccc
Luis <luisdi.28@estudiantec.cr>
2022-09-09 23:07:34 -0600

    Commit en el que se lograron implementar lo solicitado en las indicaciones.

--- Generador_archivos_desordenados.cpp
+++ Generador_archivos_desordenados.cpp
@@ -6,12 +6,12 @@
 #define PAGE_SIZE 256
 
 using namespace std;
+
 void printPage(int* page, int pageSize) {
     for (int i = 0; i < pageSize; i++) {
         cout << page[i] << endl;
     }
 }
-
 void printFile(FILE *fp) {
     int page[PAGE_SIZE];
     int readBytes = fread(&page, sizeof(int), PAGE_SIZE, fp);
@@ -26,21 +26,92 @@ void initRandomGenerator() {
     srand(time(0));
 }
 void generateFile(int size) {
-    FILE *binaryFile = fopen("binary.bin", "wb+");
-    FILE *regularFile = fopen("regular.txt", "w+");
-    int intCount = size / sizeof(int);
-    for (int i = 0; i < intCount; i++) {
-        int newValue = rand();
-        std::string newValueStr = to_string(newValue).append(",");
-        fwrite(&newValue, sizeof(int), 1, binaryFile);
-        fwrite(newValueStr.data(), newValueStr.length(), 1, regularFile);
+
+    int multiplicador = 1;
+
+    int num1 = 4;
+    int num2 = 1;
+
+    for (int cont = 1; cont < 7; cont++) {
+        for (int cantidad = 0; cantidad < 256 * multiplicador; cantidad++) {
+            FILE *binaryFile = fopen("binary.bin", "wb+");
+            FILE *regularFile = fopen("regular.txt", "w+");
+            int intCount = size / sizeof(int);
+            for (int i = 0; i < intCount; i++) {
+                int newValue = rand();
+                std::string newValueStr = to_string(newValue).append(",");
+                fwrite(&newValue, sizeof(int), 1, binaryFile);
+                fwrite(newValueStr.data(), newValueStr.length(), 1, regularFile);
+            }
+
+            fclose(regularFile);
+            fclose(binaryFile);
+
+        }
+        multiplicador = num2 * num1;
+        if (num2 >= 3) {
+            num2 += 3;
+        } else {
+            num2 += 1;
+        }
     }
-    fclose(regularFile);
-    fclose(binaryFile);
+}
+
+int partition(int page[], int start, int end){
+
+    int pivot = page[start];
+
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (page[i] <= pivot)
+            count++;
+    }
+
+    int pivotIndex = start + count;
+    swap(page[pivotIndex], page[start]);
+
+    int i = start, j = end;
+
+    while (i < pivotIndex && j > pivotIndex) {
+
+        while (page[i] <= pivot) {
+            i++;
+        }
+
+        while (page[j] > pivot) {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex) {
+            swap(page[i++], page[j--]);
+        }
+    }
+
+    return pivotIndex;
+}
+
+void quickSort(int page[], int start, int end)
+{
+
+    if (start >= end)
+        return;
+
+    int p = partition(page, start, end);
+
+    quickSort(page, start, p - 1);
+
+    quickSort(page, p + 1, end);
 }
 
 int main() {
     initRandomGenerator();
+    //quitar esto cuando se solucione lo del nombre del archivo
     generateFile(1024);
+    FILE *fp = fopen("binary.bin", "rb+");
+    if (fp == nullptr){
+        cout << "Unexpected error" << endl;
+        return 1;
+    }
+    printFile(fp);
     return 0;
 }
