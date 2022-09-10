12ff4a2b9a63263e5fdb04a19c5720eb4aec870d
Luis <luisdi.28@estudiantec.cr>
2022-09-09 21:30:57 -0600

    Commit en el que se mejoran las ideas gracias a la ayuda del profe.

--- Generador_archivos_desordenados.cpp
+++ Generador_archivos_desordenados.cpp
@@ -1,79 +1,13 @@
-#include<iostream>
-#include<stdio.h>
-#include<random>
-#include<fstream>
-#include <string>
-#include <bitset>
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 #define PAGE_SIZE 256
 
-using std::cout;
-using std::endl;
-using std::string;
-using std::ofstream;
-
 using namespace std;
-
-int* a;
-
-int crear_paginas(){
-
-    int multiplicador = 1;
-
-    int num1 = 4;
-    int num2 = 1;
-
-    for (int cont = 1; cont < 7; cont++){
-
-        ofstream outfile ("Archivo_Desordenado"+ std::to_string(cont) +".txt");
-
-        for (int cantidad = 0; cantidad < 256*multiplicador; cantidad++) {
-            int valor;
-            std::random_device rd;
-            std::mt19937 eng(rd());
-            std::uniform_int_distribution<int> dist(0, 120);
-            valor = dist(eng);
-
-            outfile << valor << ", ";
-
-            a = &valor;
-
-            FILE *fp;
-            fp = fopen("binary.bin", "wb+");
-            if (fp != nullptr){
-                for (int i = 0; i < 256; i++){
-                    fwrite (a, sizeof(int),1,fp);
-                }
-                fseek(fp,0,SEEK_SET);
-                int buffer[256];
-                int readBytes = fread(&buffer, sizeof(int), 256,fp);
-                cout << "Bytes read:" << readBytes << endl;
-
-                fclose(fp);
-                if (readBytes <= 0){
-                    return 1;
-                }
-                for (int i = 0; i < 256; i++){
-                    cout << buffer[i] << endl;
-                }
-            }
-        }
-
-        multiplicador = num2 * num1;
-        if (num2 >= 3){
-            num2+=3;
-        }
-        else{
-            num2+=1;
-        }
-
-        outfile.close();
-
-    }
-}
-
-void printPage(int* page, int pageSize){
-    for (int i = 0; i < pageSize; i++){
+void printPage(int* page, int pageSize) {
+    for (int i = 0; i < pageSize; i++) {
         cout << page[i] << endl;
     }
 }
@@ -82,20 +16,31 @@ void printFile(FILE *fp) {
     int page[PAGE_SIZE];
     int readBytes = fread(&page, sizeof(int), PAGE_SIZE, fp);
     while (readBytes > 0) {
-        cout << "Reading page" << endl;
-        printPage((int *) &page, PAGE_SIZE);
+        cout << "Reading page..." << endl;
+        printPage(page, PAGE_SIZE);
         readBytes = fread(&page, sizeof(int), PAGE_SIZE, fp);
     }
 }
 
-int main() {
-    crear_paginas();
-    FILE *fp = fopen("binary.bin", "wb+");
-    if (fp == nullptr){
-        cout << "Unexpected error" << endl;
-        return 1;
+void initRandomGenerator() {
+    srand(time(0));
+}
+void generateFile(int size) {
+    FILE *binaryFile = fopen("binary.bin", "wb+");
+    FILE *regularFile = fopen("regular.txt", "w+");
+    int intCount = size / sizeof(int);
+    for (int i = 0; i < intCount; i++) {
+        int newValue = rand();
+        std::string newValueStr = to_string(newValue).append(",");
+        fwrite(&newValue, sizeof(int), 1, binaryFile);
+        fwrite(newValueStr.data(), newValueStr.length(), 1, regularFile);
     }
-    printFile(fp);
-    fclose(fp);
+    fclose(regularFile);
+    fclose(binaryFile);
+}
+
+int main() {
+    initRandomGenerator();
+    generateFile(1024);
     return 0;
 }
